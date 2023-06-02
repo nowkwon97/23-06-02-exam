1585b0016ecd58f31c7869652d80682bec0eba00
leejaekwon <nowkwon97@gmail.com>
2023-06-02 16:21:08 +0900

    주석추가

--- index.tsx
+++ index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import * as ReactDOM from 'react-dom';
 // 함수형 컴포넌트 타입을 명시하는 React.FC
 const App: React.FC = () => {
-  return <h1>Hello World!!!!!@@!!!</h1>;
+  return <h1>Hello World!!!!!@@!!!@@</h1>;
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- server.cjs
+++ server.cjs
@@ -3,6 +3,7 @@ const app = express();
 const port = 3000;
 
 // 정적 파일 제공을 위해 'dist' 폴더를 사용합니다.
+// dist 폴더 안에 정적 파일 index.html이 존재하면 '/'경로에 나오게 된다.
 app.use(express.static('dist'));
 
 // 기본 라우트
