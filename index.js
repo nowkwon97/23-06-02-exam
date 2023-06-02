151a97eafe5fa0860f1b119203ef29594b0501f9
leejaekwon <nowkwon97@gmail.com>
2023-06-02 16:57:34 +0900

    주석 추가

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var react_1 = require("react");
 var ReactDOM = require("react-dom");
 // 함수형 컴포넌트 타입을 명시하는 React.FC
 var App = function () {
-    return <h1>Hello World!!!!!@@!!!</h1>;
+    return <h1>Hello World!!!!</h1>;
 };
 ReactDOM.render(<App />, document.getElementById('root'));
