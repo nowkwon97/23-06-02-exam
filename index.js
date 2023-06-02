6f2e7d6b43e084fc5b73031a4b456df90f2a6f35
leejaekwon <nowkwon97@gmail.com>
2023-06-02 15:50:06 +0900

    주석추가

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var react_1 = require("react");
 var ReactDOM = require("react-dom");
+// 함수형 컴포넌트 타입을 명시하는 React.FC
 var App = function () {
-    return <div>Hello World!!!!!@@</div>;
+    return <h1>Hello World!!!!!@@!!!</h1>;
 };
 ReactDOM.render(<App />, document.getElementById('root'));
