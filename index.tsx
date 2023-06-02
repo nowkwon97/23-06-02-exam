0619b787772d67c015703eba1cba1be7f68a23df
leejaekwon <nowkwon97@gmail.com>
2023-06-02 15:39:52 +0900

    index.tsx에 작업 후 webpack으로 번들링 후 express로 정상 작동 확인완료

--- index.tsx
+++ index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import * as ReactDOM from 'react-dom';
 // 함수형 컴포넌트 타입을 명시하는 React.FC
 const App: React.FC = () => {
-  return <div>Hello World!!!!!@@</div>;
+  return <div>Hello World!!!!!@@!!!</div>;
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
