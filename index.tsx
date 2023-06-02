e9098881374383734e8075f6f0386d79101a637e
leejaekwon <nowkwon97@gmail.com>
2023-06-02 15:22:59 +0900

    주석추가

--- index.tsx
+++ index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
-
-const App = () => {
-  return <div>Hello World!!!!!</div>;
+// 함수형 컴포넌트 타입을 명시하는 React.FC
+const App: React.FC = () => {
+  return <div>Hello World!!!!!@@</div>;
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
